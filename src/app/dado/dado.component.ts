4eb0dccbe29b1bafae17dc4d94110339844d0202
David Malpica <dmalpica96@hotmail.com>
2022-12-21 15:35:32 -0600

    Add styles dado component

--- src/app/dado/dado.component.ts
+++ src/app/dado/dado.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dado',
   templateUrl: './dado.component.html',
   styleUrls: ['./dado.component.css']
 })
-export class DadoComponent {
+export class DadoComponent implements OnInit {
+  @Input() valor:number=0;
+  constructor(){}
 
+  ngOnInit() {
+      
+  }
 }
